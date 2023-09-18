feb1a6da4160ea1ade2bef5c20b84a0dd62ddd77
Nicole Previd <nickprevid@gmail.com>
2023-09-17 23:08:27 -0300

    ajustando classes

--- exercicios e projetos/comecoAPI/src/main/java/restapi/Teste.java
+++ exercicios e projetos/comecoAPI/src/main/java/restapi/Teste.java
@@ -3,8 +3,6 @@ import static java.lang.System.out;
 
 public class Teste {
     private Agenda1 agenda = new Agenda1();
-    private Nomes nomes ;
-    private Scanner entrada = new Scanner(System.in);
     public Teste(Agenda a){
         agenda = a;
     }
@@ -15,14 +13,23 @@ public class Teste {
             out.println("\n MENU PRINCIPAL:");
             out.println("(1) Listar nomes");
             out.println("(2) Adicionar nome");
-            out.println("(2) Adicionar nome");
             // outros metodos id etc
             out.println("(3) Sair");
             out.println("Escolha uma opção: ");
             String op = entrada.nextLine();
             switch(op) {
                 case "1": agenda.listar(); break;
-                case "2": agenda.adicionar(); break;
+                case "2": 
+                // pergunta nome
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.println("me diga o nome q deseja adicionar")
+                String nomeRecebido = scanner1.nextLine();
+                // pergunta id
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.println("me diga o id q deseja adicionar")
+                String idRecebido = scanner2.nextLine();
+                agenda.adicionar(nomeRecebido, idRecebido); 
+                break;
                 case "3": sair = true; break;
                 default: out.println("Opção inválida!");
             }
